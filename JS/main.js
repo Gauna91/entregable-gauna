dadda66c9144a28b75e4cfe1b83a73f4dcea4b86
gisel gauna <gisellgauna@hotmail.com.ar>
2024-09-11 19:34:36 -0300

    proyectovineria

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,15 @@
 const productos = [
-{
+{   id: 1,
     nombre: "luigui bosca",
     caracteristicas: "tinto color roojo violaceo brillante.Sus aromas son intensos y amables, con notas que recuerdan a frutas rojas, y tonos algo florales y especiados",
     tipo: "malbec", 
     maridajes:"carnes, pastas",
     precio: 12400,
+    url: "./IMG/luiguibosca.jpg",
     
 },
 
-{
+{   id: 2,
     nombre:"Trumpeter",
     caracteristicas: "De un impactante color violeta, naris frutal destacando ciruelas, cerezas y notas de flores",
     tipo: "malbec", 
@@ -16,30 +17,15 @@ const productos = [
     precio: 8500,
     
 },
-{
+{   id: 3,
     nombre:"D.V Catena",
     caracteristicas: "Aporta aromas de mermelada de ciruelas maduras y moras negras, suavidad y volumen al paladar",
-    tipo: "malbec, malbec", 
+    tipo: "malbec", 
     maridajes:"carnes rojas, carnes a la parrilla, verduras salteadas",
     precio: 14900,
    
 },
-{
-    nombre:"Rutini",
-    caracteristicas: "Rojo violaceo, con matices azulados, En nariz, se destaca una gran complejidad aromatica: Notas de ciruela entremezcladas con especias que recuerdan a vainillas, anis y pimiemta negra",
-    tipo: "malbec", 
-    maridajes:"carnes vacunas, quesos duros, locro, guiso de lentejas, cordero, chivito",
-    precio: 28000,
-   
-},
-{
-    nombre:"Alamos",
-    caracteristicas: "Presenta un profundo color purpura con reflejos violetas, su aroma remite a intensos frutos negros con ligeras notas florales y de tostado",
-    tipo: "malbec", 
-    maridajes:"carnes rojas, pizzas, aves condimentadas",
-    precio: 9500,
-    
-},
+
 ]
 
 
@@ -49,15 +35,5 @@ const productos = [
 //console.log(sub)
 //sub.innerText = "hola la previa almacen y bebidas"
 
-let vinos = document.getElementById("productos")
-
-productos.forEach((producto) => {
-    let almacen = document.createElement("div")
-    almacen.className = "bibidas"
-    almacen.innerHTML = `<span>NOMBRE: ${producto.nombre}</span>
-                         <h3>TIPO: ${producto.tipo}<h3/>
-                         <h4>PRECIO: $${producto.precio}<h4/> `
-vinos.appendChild(almacen)
-}
-)
+//DOM
 
