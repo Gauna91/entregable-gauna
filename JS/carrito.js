966d1ddf6e11ed2a0a03f37722b4a1fa2274aea0
gisel gauna <gisellgauna@hotmail.com.ar>
2024-09-11 22:06:20 -0300

    proyectovineria

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,1 +1,53 @@
+const vinos = [
+    {
+        id: 1,
+        nombre:"luigui bosca",
+        caracteristicas: "tinto color roojo violaceo brillante.Sus aromas son intensos y amables, con notas que recuerdan a frutas rojas, y tonos algo florales y especiados",
+        tipo: "malbec", 
+        maridajes:"carnes, pastas",
+        precio: 12400,
+        img: "1.jpg",
+        descuento: 10
+        
+    },
+    
+    {
+        id: 2,
+        nombre:"Trumpeter",
+        caracteristicas: "De un impactante color violeta, naris frutal destacando ciruelas, cerezas y notas de flores",
+        tipo: "malbec", 
+        maridajes:"Tablas de quesos duros, carnes vacunas grilladas y pastas rellenas con salsa a base de carnes",
+        precio: 8500,
+        descuento: 10
+    },
+    {
+        id: 3,
+        nombre:"D.V Catena",
+        caracteristicas: "Aporta aromas de mermelada de ciruelas maduras y moras negras, suavidad y volumen al paladar",
+        tipo: "malbec, malbec", 
+        maridajes:"carnes rojas, carnes a la parrilla, verduras salteadas",
+        precio: 14900,
+        descuento: 10
+    },
 
+    {  
+    id: 4,
+    nombre:"Rutini",
+    caracteristicas: "Rojo violaceo, con matices azulados, En nariz, se destaca una gran complejidad aromatica: Notas de ciruela entremezcladas con especias que recuerdan a vainillas, anis y pimiemta negra",
+    tipo: "malbec", 
+    maridajes:"carnes vacunas, quesos duros, locro, guiso de lentejas, cordero, chivito",
+    precio: 28000,
+   
+    },
+    
+    {   
+    id: 5,
+    nombre:"Alamos",
+    caracteristicas: "Presenta un profundo color purpura con reflejos violetas, su aroma remite a intensos frutos negros con ligeras notas florales y de tostado",
+    tipo: "malbec", 
+    maridajes:"carnes rojas, pizzas, aves condimentadas",
+    precio: 9500,
+    
+},
+
+]
